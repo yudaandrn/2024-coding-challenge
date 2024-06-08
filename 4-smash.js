4c4bb8c0375abfa1e6ec0b621ac6c61c7f8ac092
yudaandrn <yudacavalry@gmail.com>
2024-06-08 16:22:16 +0700

    day #4

---
+++ 4-smash.js
@@ -0,0 +1,11 @@
+// Coding Challenge 2024
+// 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+// function smash(words) {
+//   return words.join(' ');
+// }
+
+const smash = (words) => words.join(" ");
+
+console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
