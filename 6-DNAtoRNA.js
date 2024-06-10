d18fb019ac85bf64b7e095b8196652c1d23bc42b
yudaandrn <yudacavalry@gmail.com>
2024-06-10 16:47:32 +0700

    Day 6/366

---
+++ 6-DNAtoRNA.js
@@ -0,0 +1,25 @@
+// Coding Challenge 2024
+// 6/366
+// https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+
+// function DNAtoRNA(dna) {
+//   let rna = "";
+//   for (let i = 0; i < dna.length; i++) {
+//     rna += dna[i] === "T" ? "U" : dna[i];
+//   }
+//   return rna;
+// }
+
+// const DNAtoRNA = (dna) =>
+//   dna
+//     .split("")
+//     .map((el) => (el === "T" ? "U" : el))
+//     .join("");
+
+// const DNAtoRNA = (dna) => dna.replace(/T/g, 'U');
+
+// const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
+
+const DNAtoRNA = (dna) => dna.split("T").join("U");
+
+console.log(DNAtoRNA("GCATTTT"));
