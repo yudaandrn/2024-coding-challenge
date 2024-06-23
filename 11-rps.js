e308a13e046db157a3b55012991cbcd2a3973c10
yudaandrn <yudacavalry@gmail.com>
2024-06-23 17:05:36 +0700

    Day 11/366

--- 11-rps.js
+++ 11-rps.js
@@ -2,6 +2,39 @@
 // 11/366
 // https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
 
-const rps = (p1, p2) => {};
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return "Draw!";
+//   if (p1 === "rock" && p2 === "scissors") {
+//     return "Player 1 won!";
+//   } else if (p1 === "scissors" && p2 === "paper") {
+//     return "Player 1 won!";
+//   } else if (p1 === "paper" && p2 === "rock") {
+//     return "Player 1 won!";
+//   } else {
+//     return "Player 2 won!";
+//   }
+// };
 
-console.log(rps("rock", "scissors"));
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return "Draw!";
+//   if (
+//     (p1 === "rock" && p2 === "scissors") ||
+//     (p1 === "scissors" && p2 === "paper") ||
+//     (p1 === "paper" && p2 === "rock")
+//   ) {
+//     return "Player 1 won!";
+//   } else {
+//     return "Player 2 won!";
+//   }
+// };
+
+const rps = (p1, p2) =>
+  p1 === p2
+    ? "Draw!"
+    : (p1 === "rock" && p2 === "scissors") ||
+      (p1 === "scissors" && p2 === "paper") ||
+      (p1 === "paper" && p2 === "rock")
+    ? "Player 1 won!"
+    : "Player 2 won!";
+
+console.log(rps("paper", "paper"));
