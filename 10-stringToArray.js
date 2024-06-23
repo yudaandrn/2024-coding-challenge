382f8be68798f4755f11cff205c670b69bb923d0
yudaandrn <yudacavalry@gmail.com>
2024-06-23 16:30:34 +0700

    Day 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,6 @@
+// Coding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+const stringToArray = (string) => string.split(" ");
+// code code code
